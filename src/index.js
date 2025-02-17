d0e028b679115e6271dbaf6ee5c18f66470671a6
raj <razbc12@gmail.com>
2025-02-17 20:42:17 +0545

    Create html skeleton

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 "use strict";
-import "styles.css";
+import "./styles.css";
+
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const rePassword = document.querySelector("#re-password");
+const country = document.querySelector("#country");
+const code = document.querySelector("#code");
+const submit = document.querySelector(".submit");
+
+const emailError = document.querySelector("#email + span.error");
+
+function validateEmail() {
+  const emailConstraint = new RegExp();
+}
